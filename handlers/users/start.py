eae2318cb7e1bae39ce17a89eca395b5dc496282
Olim <olim@gmail.com>
2024-06-10 11:49:07 +0500

    fix

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,18 +7,24 @@ from data.config import ADMINS
 from loader import dp, db, bot
 from keyboards.default.SelectLanguage import start
 
+
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     name = message.from_user.full_name
     # Foydalanuvchini bazaga qo'shamiz
     try:
-        db.add_user(id=message.from_user.id,
-                    name=name)
+        user = db.select_user(id=message.from_user.id)
+        if user:
+            pass
+        else:
+            db.add_user(id=message.from_user.id, name=name)
     except sqlite3.IntegrityError as err:
         await bot.send_message(chat_id=ADMINS[0], text=err)
 
     await message.answer("<b>Tilni tanlang / Выберите язык </b>", reply_markup=start)
     # Adminga xabar beramiz
-    #count = db.count_users()[0]
-   # msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    #await bot.send_message(chat_id=ADMINS[0], text=msg)
+    # count = db.count_users()[0]
+
+
+# msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+# await bot.send_message(chat_id=ADMINS[0], text=msg)
